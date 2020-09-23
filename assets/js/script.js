fa01a4fd3beb076c04208f98c1fa0a7797c825f9
Chloe Ng <cngchloen@gmail.com>
2020-09-22 21:05:03 -0700

    detail tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     items: '.grid-item'
   });
 
-
+  console.log("graphic")
 
   // by default, hide the closeup
 
@@ -130,13 +130,13 @@ $(document).ready(function() {
   modal.append(modalX); 
 
 
-  let handSvg = document.createElementNS("http://www.w3.org/2000/svg","svg");
-  $(".modal").append(handSvg);
-  handSvg.setAttributeNS(null, 'width', 48);
-  handSvg.setAttributeNS(null, 'height', 48);
-  handSvg.setAttributeNS(null, 'viewBox', '0 0 48 48');
-  handSvg.innerHTML = '<path fill-rule="evenodd" clip-rule="evenodd" d="M17.7756 22.3904C13.7747 23.9776 9.24586 22.359 7.21871 18.7698C7.03957 18.5027 6.86043 18.2183 6.68757 17.9103C5.13186 15.1634 3.02771 13.7821 2.58771 13.581C2.14928 13.3783 0.595142 12.613 1.09957 11.6921C1.38086 11.1798 2.16343 10.8011 2.87843 10.7257C4.00043 10.4806 5.89714 11.0337 7.77186 12.2217L4.05386 3.41855C3.74114 2.67684 3.98943 1.85027 4.60857 1.37884C4.61171 1.37569 4.61643 1.37255 4.62114 1.36941C4.67614 1.32698 4.73428 1.28927 4.79557 1.25312C4.80971 1.24684 4.82071 1.23898 4.83486 1.23112C4.90086 1.19498 4.97 1.16041 5.04228 1.13212C5.04386 1.13055 5.04543 1.13055 5.04543 1.13055C5.04543 1.13055 5.04543 1.13055 5.047 1.12898C5.11928 1.10069 5.19314 1.07869 5.267 1.05984C5.28114 1.05669 5.29528 1.05355 5.30943 1.04884C5.38014 1.03469 5.44928 1.02055 5.51843 1.01269C5.52471 1.01269 5.52943 1.01112 5.53414 1.01112C6.31671 0.927837 7.093 1.34741 7.40571 2.08755L9.59157 7.26069C9.64343 6.62269 10.0551 6.03498 10.7089 5.77569C11.6329 5.40955 12.6889 5.83855 13.0691 6.73427C13.0707 6.74212 13.0739 6.75155 13.077 6.75941C13.143 6.13555 13.5516 5.56512 14.1911 5.31212C15.1167 4.94598 16.1727 5.37341 16.5514 6.27069C16.5671 6.30527 16.5781 6.34141 16.5907 6.37912C16.6803 5.78355 17.0794 5.24927 17.6954 5.00569C18.6194 4.63955 19.6189 4.96955 19.9991 5.86684L20.0007 5.87155C20.0007 5.87155 21.1149 8.03855 22.3657 11.6513C22.4129 11.7911 22.4569 11.931 22.4946 12.0724C24.0267 16.1613 21.9666 20.7278 17.7756 22.3904Z" fill="#111111"/>'
-  handSvg.setAttribute("class","handSvg");
+  // let handSvg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+  // $(".modal").append(handSvg);
+  // handSvg.setAttributeNS(null, 'width', 48);
+  // handSvg.setAttributeNS(null, 'height', 48);
+  // handSvg.setAttributeNS(null, 'viewBox', '0 0 48 48');
+  // handSvg.innerHTML = '<path fill-rule="evenodd" clip-rule="evenodd" d="M17.7756 22.3904C13.7747 23.9776 9.24586 22.359 7.21871 18.7698C7.03957 18.5027 6.86043 18.2183 6.68757 17.9103C5.13186 15.1634 3.02771 13.7821 2.58771 13.581C2.14928 13.3783 0.595142 12.613 1.09957 11.6921C1.38086 11.1798 2.16343 10.8011 2.87843 10.7257C4.00043 10.4806 5.89714 11.0337 7.77186 12.2217L4.05386 3.41855C3.74114 2.67684 3.98943 1.85027 4.60857 1.37884C4.61171 1.37569 4.61643 1.37255 4.62114 1.36941C4.67614 1.32698 4.73428 1.28927 4.79557 1.25312C4.80971 1.24684 4.82071 1.23898 4.83486 1.23112C4.90086 1.19498 4.97 1.16041 5.04228 1.13212C5.04386 1.13055 5.04543 1.13055 5.04543 1.13055C5.04543 1.13055 5.04543 1.13055 5.047 1.12898C5.11928 1.10069 5.19314 1.07869 5.267 1.05984C5.28114 1.05669 5.29528 1.05355 5.30943 1.04884C5.38014 1.03469 5.44928 1.02055 5.51843 1.01269C5.52471 1.01269 5.52943 1.01112 5.53414 1.01112C6.31671 0.927837 7.093 1.34741 7.40571 2.08755L9.59157 7.26069C9.64343 6.62269 10.0551 6.03498 10.7089 5.77569C11.6329 5.40955 12.6889 5.83855 13.0691 6.73427C13.0707 6.74212 13.0739 6.75155 13.077 6.75941C13.143 6.13555 13.5516 5.56512 14.1911 5.31212C15.1167 4.94598 16.1727 5.37341 16.5514 6.27069C16.5671 6.30527 16.5781 6.34141 16.5907 6.37912C16.6803 5.78355 17.0794 5.24927 17.6954 5.00569C18.6194 4.63955 19.6189 4.96955 19.9991 5.86684L20.0007 5.87155C20.0007 5.87155 21.1149 8.03855 22.3657 11.6513C22.4129 11.7911 22.4569 11.931 22.4946 12.0724C24.0267 16.1613 21.9666 20.7278 17.7756 22.3904Z" fill="#111111"/>'
+  // handSvg.setAttribute("class","handSvg");
 
 
   //make modal masonry
@@ -148,12 +148,13 @@ $(document).ready(function() {
   for (let i = 0; i < 12; i++) {
     let modalMasonryPin = document.createElement("div");
 
-    if (i == 5) {
+    if (i == 6) {
       modalMasonryPin.className = "item item-main"
-    } else if (i == 1 || i == 3 || i == 7 || i ==8) {
-      modalMasonryPin.className = "item item-short";
-    } else if (i == 4) {
+      modalMasonryPin.id = "item-main"
+    } else if (i == 1 || i == 4) {
       modalMasonryPin.className = "item item-medium";
+    } else if (i == 3 || i == 2 || i == 5 || i == 9 ) {
+      modalMasonryPin.className = "item item-short";
     } else {
       modalMasonryPin.className = "item";
 
@@ -162,11 +163,28 @@ $(document).ready(function() {
     modalMasonry.append(modalMasonryPin);
   }
 
+
   //make modal save
 
   let modalSave = document.createElement("div");
   modalSave.className = "modal-save";
   modalSave.innerHTML = "Save";
+
+  // lottie starburst
+
+  let pinGraphic = document.createElement("div");
+  pinGraphic.className = "pinGraphic";
+  pinGraphic.innerHTML = "test";
+  $(".item-main").append(pinGraphic);
+
+
+  // let cookingAnimate = lottie.loadAnimation({
+  //   container: pinGraphic, // the dom element that will contain the animation
+  //   renderer: 'svg',
+  //   loop: true,odal
+  //   autoplay: true,
+  //   path: 'https://assets10.lottiefiles.com/private_files/lf30_gjaElU.json' // the path to the animation json
+  // });
 
 
   function firstHFCard() {
@@ -235,7 +253,7 @@ $(document).ready(function() {
 
       // re append the hand!
 
-      $(".modal").append(handSvg);
+      // $(".modal").append(handSvg);
 
 
     }
@@ -273,20 +291,24 @@ $(document).ready(function() {
     $(".item-main").css("animation-iteration-count", "infinite");
     $(".item-main").css("animation-timing-function", "cubic-bezier(.63, .09, .3, .43");
 
+    // // lottie animation
+
+    // cookingAnimate.goToAndPlay(0)
+
 
     // this is the first step
     $(modalDot1).addClass("modal-dot-active");
     $(modalDot2).removeClass("modal-dot-active");
     $(".modal-content-cards").css("margin-left", "200%")
 
-    // pointer animation
-    $(".handSvg").css("position", "absolute");
-    $(".handSvg").css("top", "24%");
-    $(".handSvg").css("left", "60%");
-    $(".handSvg").css("animation-name", "clickingArc");
-    $(".handSvg").css("animation-duration", "1.5s");
-    $(".handSvg").css("animation-iteration-count", "infinite");
-    $(".handSvg").css("animation-direction", "alternate");
+    // // pointer animation
+    // $(".handSvg").css("position", "absolute");
+    // $(".handSvg").css("top", "24%");
+    // $(".handSvg").css("left", "60%");
+    // $(".handSvg").css("animation-name", "clickingArc");
+    // $(".handSvg").css("animation-duration", "1.5s");
+    // $(".handSvg").css("animation-iteration-count", "infinite");
+    // $(".handSvg").css("animation-direction", "alternate");
 
 
 
@@ -387,7 +409,7 @@ $(document).ready(function() {
 
       // re append the hand!
 
-      $(".modal").append(handSvg);
+      // $(".modal").append(handSvg);
 
       // slide in masonry
       var slideInMasonry = anime({
@@ -416,6 +438,7 @@ $(document).ready(function() {
     var scaleUp = anime({
       targets: '.masonry',
       scale: 2.5,
+      translateY: 16,
       easing: 'cubicBezier(0.66, 0.025, 0.33, 0.975)'
 
     });
@@ -439,12 +462,12 @@ $(document).ready(function() {
     $(".modal-save").css("animation-timing-function", "cubic-bezier(.63, .09, .3, .43");
 
 
-    $(".handSvg").css("top", "28%");
-    $(".handSvg").css("left", "64%");
-    $(".handSvg").css("animation-name", "clicking");
-    $(".handSvg").css("animation-duration", "1.5s");
-    $(".handSvg").css("animation-iteration-count", "infinite");
-    $(".handSvg").css("animation-direction", "normal");
+    // $(".handSvg").css("top", "28%");
+    // $(".handSvg").css("left", "64%");
+    // $(".handSvg").css("animation-name", "clicking");
+    // $(".handSvg").css("animation-duration", "1.5s");
+    // $(".handSvg").css("animation-iteration-count", "infinite");
+    // $(".handSvg").css("animation-direction", "normal");
 
 
 
@@ -494,7 +517,7 @@ $(document).ready(function() {
 
       // re append the hand!
 
-      $(".modal").append(handSvg);
+      // $(".modal").append(handSvg);
 
 
     } 
@@ -565,12 +588,12 @@ $(document).ready(function() {
 
 
 
-    $(".handSvg").css("top", "28%");
-    $(".handSvg").css("left", "74%");
-    $(".handSvg").css("animation-name", "clicking");
-    $(".handSvg").css("animation-duration", "1.5s");
-    $(".handSvg").css("animation-iteration-count", "infinite");
-    $(".handSvg").css("animation-direction", "normal");
+    // $(".handSvg").css("top", "28%");
+    // $(".handSvg").css("left", "74%");
+    // $(".handSvg").css("animation-name", "clicking");
+    // $(".handSvg").css("animation-duration", "1.5s");
+    // $(".handSvg").css("animation-iteration-count", "infinite");
+    // $(".handSvg").css("animation-direction", "normal");
 
 
 
@@ -592,19 +615,21 @@ $(document).ready(function() {
   modalBoardRep2.className = "modal-board-rep-2";
   masonryBg.append(modalBoardRep2);
 
-  let starburst = document.createElement("div");
-  starburst.className = "starburst";
-  masonryBg.append(starburst);
+  // let starburst = document.createElement("div");
+  // starburst.className = "starburst";
+  // masonryBg.append(starburst);
 
-  // lottie starburst
+  // // lottie starburst
 
-  let starburstAnimate = lottie.loadAnimation({
-    container: starburst, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    path: 'https://assets1.lottiefiles.com/packages/lf20_hbkV3h.json' // the path to the animation json
-  });
+  // let starburstAnimate = lottie.loadAnimation({
+  //   container: starburst, // the dom element that will contain the animation
+  //   renderer: 'svg',
+  //   loop: false,
+  //   autoplay: false,
+  //   path: 'https://assets1.lottiefiles.com/packages/lf20_hbkV3h.json' // the path to the animation json
+  // });
+
+
 
 
   // animate board name up 
@@ -665,8 +690,7 @@ $(document).ready(function() {
     $(".item").not(".item-main").css("opacity", "0");
 
     // remove hand
-    handSvg.remove();
-
+    // handSvg.remove();
 
     // border-radius & width transition with CSS is smoother
 
@@ -687,7 +711,7 @@ $(document).ready(function() {
     toBoard
       .add({
         targets: '.item-main',
-        translateY: -8,
+        translateY: -19,
         translateX: -31,
       })
       .add({
@@ -708,9 +732,9 @@ $(document).ready(function() {
 
     // play lottie
 
-    setTimeout(function(){ 
-      starburstAnimate.goToAndPlay(0)
-    }, 1900);
+    // setTimeout(function(){ 
+    //   starburstAnimate.goToAndPlay(0)
+    // }, 1900);
     
 
   }
